8c0eb013e7459b3b84e4371e5dc95148bda9d641
hritik kumar <hritik.7827@gmail.com>
2025-08-13 12:58:27 +0530

    Added one to many relation between booking and driver, booking and passenger

---
+++ src/main/java/com/project/uber/model/Driver.java
@@ -0,0 +1,26 @@
+package com.project.uber.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Driver extends BaseModel {
+
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String licenseNumber;
+
+    @OneToMany(mappedBy = "driver")
+    private List<Booking> bookings = new ArrayList<>();
+}
